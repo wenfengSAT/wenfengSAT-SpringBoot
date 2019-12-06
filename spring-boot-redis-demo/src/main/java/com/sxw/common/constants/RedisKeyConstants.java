940a0a87c3dd26e52fbfa1fd464ad4ebe6985b8d
Huang Wenfeng <Wenfeng.Huang@desay-svautomotive.com>
2019-12-06 14:53:33 +0800

    springboot redis消息订阅应用demo

---
+++ spring-boot-redis-demo/src/main/java/com/sxw/common/constants/RedisKeyConstants.java
@@ -0,0 +1,11 @@
+package com.sxw.common.constants;
+
+public class RedisKeyConstants {
+    /**
+     * 用来计算消费队列的自增key
+     */
+    public static final String SUBSCRIBE_QUEUE_INCR_KEY = "sqik";
+
+    public static final String SUBSCRIBE_QUEUE_KEY = "sqk_";
+
+}
---
+++ spring-boot-redis-demo/src/main/java/com/sxw/service/MessagePushService.java
@@ -0,0 +1,9 @@
+package com.sxw.service;
+
+public interface MessagePushService {
+    /**
+     * 推送消息
+     * @return
+     */
+    boolean pushMsg(Object set);
+}
